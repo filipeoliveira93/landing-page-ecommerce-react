7c5d4f1eb578d635785bf2d3afcf6333d924a405
Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2023-04-29 17:22:59 -0300

    bannercategories

---
+++ src/bannercategories.jsx
@@ -0,0 +1,48 @@
+function Bannercategories() {
+	const categories = [
+		{ 
+			id: 1,
+			name: 'For Men',
+			description: 'Clothing Bundle Deal',
+			cta: 'View Deal',
+			img: '/src/assets/png/formen.png'
+		}, 
+		{
+			id:2,
+			name: 'For Woman',
+			description: 'Clothing Bundle Deal',
+			cta: 'View Deal',
+			img: '/src/assets/png/forwoman.png'
+		},
+		{
+			id:3,
+			name: 'Accessories',
+			description: 'Handle Bag Collection',
+			cta: 'View Deal',
+			img: '/src/assets/png/accessories.png'
+		}
+
+	]
+
+	const itemlist = categories.map(item =>(
+		<div key={item.id} className=" bg-center bg-cover flex flex-col pt-[145px] w-[407px] h-[267px]" style={{backgroundImage: `url(${item.img})`}}>
+			<p className="font-semibold text-[16px]">{item.name}</p>
+			<p className="font-semibold text-[16px]">{item.description}</p>
+			<a className=" font-medium text-[13px]">{item.cta}</a>
+		</div>
+	)) 
+
+	return (
+		<>
+			<div className="flex flex-row justify-center gap-3 my-2">
+			{itemlist}
+			</div>
+		</>
+	);
+}
+
+<style>
+	.button
+</style>
+
+export default Bannercategories;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './header.jsx'
 import Banner from './banner.jsx'
+import Bannercategories from './bannercategories.jsx'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode clasname="flex">
     <Header/>
     <Banner/>
+    <Bannercategories/>
     
   </React.StrictMode>,
 )
